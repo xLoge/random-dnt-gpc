8b50a3fd5ee37fecc474f228a512e85e2bcf2db2
Loge <85043963+xLoge@users.noreply.github.com>
2024-06-15 14:38:45 +0100

    Update overwrite_navigator.js

--- overwrite_navigator.js
+++ overwrite_navigator.js
@@ -1,35 +1,32 @@
-browser.runtime.sendMessage({ action: 'get-dnt-state' }).then(function(response) {
-  let enableDNT = response.state;
-  if (enableDNT === 'disabled') {
-    return;
+browser.runtime.sendMessage({ action: 'get-dnt-gpc-state' }).then(function(response) {
+  let enableDNT = response.states.DNT;
+  let enableGPC = response.states.GPC;
+
+  if (enableDNT !== -1)
+  {
+    let script = document.createElement('script');
+    script.textContent = `
+      Object.defineProperty(navigator, 'doNotTrack', {
+        value: ${enableDNT ? '1' : ''},
+        writable: false,
+        configurable: false
+      });
+    `;
+    document.documentElement.appendChild(script);
+    script.remove();
   }
 
-  var script = document.createElement('script');
-  script.textContent = `
-    Object.defineProperty(navigator, 'doNotTrack', {
-      value: ${enableDNT ? '1' : ''},
-      writable: false,
-      configurable: false
-    });
-  `;
-  document.documentElement.appendChild(script);
-  script.remove();
-});
-
-browser.runtime.sendMessage({ action: 'get-gpc-state' }).then(function(response) {
-  let enableGPC = response.state;
-  if (enableGPC === 'disabled') {
-    return;
+  if (enableGPC !== -1)
+  {
+    let script = document.createElement('script');
+    script.textContent = `
+      Object.defineProperty(navigator, 'globalPrivacyControl', {
+        value: ${enableGPC ? true : false},
+        writable: false,
+        configurable: false
+      });
+    `;
+    document.documentElement.appendChild(script);
+    script.remove();
   }
-
-  var script = document.createElement('script');
-  script.textContent = `
-    Object.defineProperty(navigator, 'globalPrivacyControl', {
-      value: ${enableGPC ? true : false},
-      writable: false,
-      configurable: false
-    });
-  `;
-  document.documentElement.appendChild(script);
-  script.remove();
 });
